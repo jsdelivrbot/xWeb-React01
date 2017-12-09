920fea5ab373c887af80cca156eed216467d27f6
TheStunnr <thestunnr@gmail.com>
2017-12-10 03:21:51 +0800

    .

---
+++ src/components/search_bar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+  	return <input />;
+  }
+}
+
+export default SearchBar;
+
+/* 
+const SearchBar = () => {
+	return <input />
+};
+
+export default SearchBar;
+
+Test for git*/
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// YouTube API
+const API_KEY = 'AIzaSyBC8UO09-ja-vhWp9Tk6QWIai5x-hwYhpg'; 
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce some HTML
+const App = () => {
+	return (
+		<div>
+		  <SearchBar />
+
+		</div>
+	);	
+} 
+ 
+ReactDOM.render(<App />, document.querySelector('.container'));
