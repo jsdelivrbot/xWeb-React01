64f8dc6306b138f99bac8247b1d09b6bdd54a9a0
TheStunnr <thestunnr@gmail.com>
2017-12-11 13:49:13 +0800

    further manipulations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 // YouTube API
-const API_KEY = 'AIzaSyBC8UO09-ja-vhWp9Tk6QWIai5x-hwYhpg'; 
+const API_KEY = 'AIzaSyDxjgry2HZl0vtlCFTkz_RDDFwwIFIlFdE'; 
+
+// Test Search
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+});
 
 // Create a new component. This component should produce some HTML
 const App = () => {
